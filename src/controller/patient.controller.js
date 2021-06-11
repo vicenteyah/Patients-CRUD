ac122e0998b1551e2cd3077dc0ff3f3eacf846ac
vicenteyah <vicente.yah.dzul@icloud.com>
2021-06-11 02:54:26 -0500

    normal functions changed to arrow functions

--- src/controller/patient.controller.js
+++ src/controller/patient.controller.js
@@ -1,18 +1,18 @@
 const db = require('../models')
 const Patient = db.Patient
 
-async function createPatient(req,res){
+const createPatient = async(req,res) => {
     let payload = req.body
     try {
         const newPatient = await Patient.create(payload)
-        res.status(201).send({user:newPatient, message:'patient created successfully'})
+        res.status(201).send({uuid:newPatient.id, message:'patient created successfully'})
     } catch (error) {
         console.log(error)
         res.status(500).send({message: err.message })
     }
 }
 
-async function getPatients(req, res){
+const getPatients = async(req, res) => {
     try {
         const patients = await Patient.findAll()
         res.status(200).send({patients:patients})
