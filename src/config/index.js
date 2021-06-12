ff7ac9187a17d30fd6128823cfde5a16886837ee
vicenteyah <vicente.yah.dzul@icloud.com>
2021-06-11 23:49:35 -0500

    config was optimized

--- src/config/index.js
+++ src/config/index.js
@@ -1,4 +1,23 @@
-module.exports = {
-    appConfig: require('./app.config'),
-    dbConfig: require('./db.config')
+require('dotenv').config()
+
+const config ={
+    appConfig: { 
+        host: process.env.APP_HOST,
+        port: process.env.APP_PORT,
+    },
+    dbConfig: {
+        HOST: process.env.DB_HOST,
+        USER: process.env.DB_USER,
+        PASSWORD: process.env.DB_PASSWORD,
+        DB: process.env.DB_NAME,
+        dialect: 'mysql',
+        pool: {
+            max: 100,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    }
 }
+
+module.exports = config
