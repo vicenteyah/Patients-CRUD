10beddb9348fcc52fc8600576de68a2681f11bbd
vicenteyah <vicente.yah.dzul@icloud.com>
2021-06-11 23:56:28 -0500

    new endpoints were added to app.js file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,11 @@ const { Router } = require('express')
 const api = Router()
 
 const apiPatient = require('./patient.routes')
+const apiTutor = require('./tutor.routes')
+const apiHospital = require('./hospital.routes')
 
 api.use(apiPatient)
+api.use(apiTutor)
+api.use(apiHospital)
 
 module.exports = api
